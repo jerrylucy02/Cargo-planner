fcee3936a2b6ed8b97078bbbbd79c306e4888128
jerrylucy02 <jerrywong@gmx.de>
2026-03-17 10:51:39 +0100

    Add files via upload
    initial upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{padding:20}}>
+      <h1>Cargo Load Planner</h1>
+      <p>App läuft ✔️</p>
+      <p>Hier wird deine Loadplanung angezeigt.</p>
+    </div>
+  );
+}
